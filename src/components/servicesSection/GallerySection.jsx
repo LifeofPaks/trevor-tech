5505ffb814751130fc3b01fbceee8d0499f4a8ff
lifeofpaks <chukwudi.promise@yahoo.com>
2025-10-25 14:23:15 +0100

    udpated gallery section

---
+++ src/components/servicesSection/GallerySection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const GallerySection = () => {
+  return (
+    <div className="!pt-[5rem] flex items-center">
+      <div>
+        <h1 className=" font-extrabold text-[2rem]" id="services">
+          Financial & Crypto Recovery Solutions
+        </h1>
+        <p className="  text-gray-600 !mt-4 text-[1.1rem]">
+          Take back control of money, credit, and digital assets, recover
+          scammed crypto, boost credit scores, load debit cards, and stop
+          financial blackmail. Full transparency into bank logs, transactions,
+          and hidden accounts.
+        </p>
+      </div>
+      <img src="" alt="" />
+    </div>
+  );
+}
+
+export default GallerySection
--- src/components/servicesSection/Services.jsx
+++ src/components/servicesSection/Services.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import PhoneSection from './PhoneSection';
+import React from "react";
+import PhoneSection from "./PhoneSection";
+import GallerySection from "./GallerySection";
 
 const Services = () => {
   return (
     <div className="bg-white">
-          <div className="!p-[5rem] max-w-[1400px] !mx-auto">
-<PhoneSection />
+      <div className="!p-[5rem] max-w-[1400px] !mx-auto">
+              <PhoneSection />
+              <GallerySection/>
       </div>
     </div>
   );
-}
+};
 
-export default Services
+export default Services;
