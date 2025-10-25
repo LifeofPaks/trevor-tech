23d2dc0708d6000652593892e8bd5d0b6d9107ef
lifeofpaks <chukwudi.promise@yahoo.com>
2025-10-25 19:58:22 +0100

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
 import DemoPage from "./pages/demo/DemoPage";
 import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 import DashboardPage from "./pages/demo/DashboardPage";
 
-
 function App() {
   return (
     <>
       <Routes>
+        {/* Landing Page */}
         <Route path="/" element={<LandingPage />} />
+
+        {/* Demo Section */}
         <Route path="/demo" element={<DemoPage />}>
+          {/* Redirect /demo → /demo/dashboard */}
+          <Route index element={<Navigate to="dashboard" replace />} />
           <Route path="dashboard" element={<DashboardPage />} />
         </Route>
       </Routes>
+
       <ScrollToTop />
     </>
   );
 }
-
 
 export default App;
