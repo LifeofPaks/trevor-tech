5d8c23ce0daac6b2d85e88b420fb2c6c627e9125
lifeofpaks <chukwudi.promise@yahoo.com>
2025-10-25 11:37:21 +0100

    added logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Logo1 from "../../assets/logo-1.png";
 import Logo3 from "../../assets/logo-3.png";
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <div className="flex items-center flex-col">
+    <Link to="/" className="flex items-center flex-col">
       <div className="flex items-center -mb-[5px]!">
         <img src={Logo3} alt="logo-image" className="w-[40px]" />
         <img
@@ -17,7 +18,7 @@ const Logo = () => {
       <div className=" logo-font text-[10px]">
         Trevor <span className="text-red-400 -ml-[3px]!">Tech</span>
       </div>
-    </div>
+    </Link>
   );
 }
 
