469ae6c9af814f4bb7bd6dac26099538e3072921
lifeofpaks <chukwudi.promise@yahoo.com>
2025-10-25 12:04:50 +0100

    updated navbar

--- src/components/navbar/Navbarr.jsx
+++ src/components/navbar/Navbarr.jsx
@@ -6,7 +6,7 @@ import Logo from '../logo/Logo';
 const Navbarr = () => {
   return (
     <nav className=" w-full flex justify-center mx-auto!">
-      <div className="flex items-center justify-between text-gray-800 !px-12 !py-4 w-[1000px] fixed top-4 left-0 right-0 mx-auto! z-50 bg-white/30 backdrop-blur-lg shadow-lg rounded-2xl">
+      <div className="flex items-center justify-between text-gray-800 !px-12 !py-4 w-[1000px] fixed top-4 left-0 right-0 mx-auto! z-50 bg-white/50 backdrop-blur-md shadow-lg rounded-2xl">
         <Logo />
         <ul className="flex items-center gap-8 text-[14px] font-medium">
           <li>
