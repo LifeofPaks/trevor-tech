e39a882bf77b047171d1c69cd66a0011decf2372
lifeofpaks <chukwudi.promise@yahoo.com>
2025-11-03 11:31:35 +0100

    udpated lang switcher position on demo page

--- src/languageSwitcher/LanguageSwitcher.jsx
+++ src/languageSwitcher/LanguageSwitcher.jsx
@@ -36,7 +36,9 @@ const LanguageSwitcher = () => {
 
   return (
     <div
-      className="!fixed lg:!top-10 !top-22 !right-5 !z-[1000]"
+      className={`!fixed ${
+        isDemoPage ? "lg:!top-5" : "lg:!top-10"
+      }  !top-22 !right-5 !z-[1000]`}
       style={{
         direction:
           i18n.language === "ar" || i18n.language === "ur" ? "rtl" : "ltr",
