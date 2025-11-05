01a95bcde2d28ab27544c326753684b93c36fb00
lifeofpaks <chukwudi.promise@yahoo.com>
2025-11-05 09:23:12 +0100

    translated demo footer component

--- src/components/demo/DemoFooter.jsx
+++ src/components/demo/DemoFooter.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 const DemoFooter = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="text-center !py-6 sm:!py-8 text-[10px] sm:text-xs text-slate-500">
-      Copyright © 2025 <span className="font-bold">TrevorTech</span> All rights
-      reserved.
+      {t("df.copyright", { year: 2025 })}{" "}
+      <span className="font-bold">TrevorTech</span> {t("df.rights_reserved")}
     </div>
   );
-}
+};
 
-export default DemoFooter
+export default DemoFooter;
